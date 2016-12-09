cfe394ee487593f4789bcb01f67a554cfa726689
Saša Savić <savic-sasa@outlook.com>
2016-12-09 22:48:05 +0100

    Add basic pagination on post listing

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -15,7 +15,8 @@ class PostsController extends Controller
      */
     public function index()
     {
-        return view('posts.index');
+        $posts = Post::orderBy('created_at', 'desc')->paginate(5);
+        return view('posts.index')->with('posts', $posts);
     }
 
     /**
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -9,7 +9,22 @@
   <h3 class="post-subtitle">
     {{ $post->content }}
   </h3>
-    <p class="post-meta">Posted on {{ $post->date }}</p>
+    <p class="post-meta">Posted on {{ $post->created_at }}</p>
 </div>
 <hr>
 @endforeach
+@if ($posts->total() > $posts->perPage())
+<ul class="pager">
+  @if ($posts->nextPageUrl())
+  <li class="next">
+    <a href="{{ $posts->nextPageUrl() }}">Older Posts &rarr;</a>
+  </li>
+  @endif
+  @if ($posts->previousPageUrl())
+  <li class="previous">
+    <a href="{{ $posts->previousPageUrl() }}">&larr; Newer Posts</a>
+  </li>
+  @endif
+</ul>
+@endif
+@stop
