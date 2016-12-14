7e8542e0d12650f9c53b9c732fb76e7b85f6ee3c
Saša Savić <savic-sasa@outlook.com>
2016-12-14 23:19:44 +0100

    Ugrade editor with more features
    Add font size chooser, foreground and background color picker,
    underline button.

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -35,21 +35,25 @@
     <button type='submit' name='submit' id='submit' class='btn btn-primary' value='Submit'>Submit</button>
   </div>
 </form>
-<script src="http://cdn.tinymce.com/4/plugins/openlink/plugin.min.js"></script>
 <script src="http://cdn.tinymce.com/4/tinymce.min.js"></script>
-<script>tinymce.init(
-  {
-    selector: ".has_editor",
-    menubar: false,
-      plugins: [
-        "advlist autolink lists link image charmap print preview anchor",
-        "searchreplace visualblocks code fullscreen",
-        "insertdatetime media table contextmenu paste imagetools"
-    ],
-    toolbar: [
-      "undo redo | styleselect | bold italic underline | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image",
-    ]
+<script>
+tinymce.init(
+{
+  selector: ".has_editor",
+  menubar: false,
+    plugins: [
+      "lists link image textcolor",
+  ],
+  formats: {
+    forecolor: {inline: 'span', classes: 'forecolor', styles:{color: "%value"}}
+  },
+  textcolor_map: [
+    "0c0934", "Dark Blue", "152845", "Blue", "CACEE8", "Sky Blue", "E5DED8", "Mushy White", "2F0000", "Cherry"
+  ],
+  toolbar: [
+    "undo redo | styleselect | bold italic underline | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image",
+    "fontsizeselect | forecolor backcolor"
+  ]
 })
-
 </script>
 @stop
