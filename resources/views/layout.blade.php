074dd10bff526b8b6dbe771f967876af51fb2709
Saša Savić <savic-sasa@outlook.com>
2016-12-18 21:24:08 +0100

    Make dropdown work on hover

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -36,8 +36,7 @@
           <li>
           </li>
           <li role='presentation' class='dropdown'>
-            <a class='dropdown-toggle' data-toggle='dropdown' href='#'
-               role='button' aria-haspopup='true' aria-expanded='false'>
+            <a href='#' class='dd'>
               Contact <span class='caret'></span>
             </a>
             <ul class='dropdown-menu'>
